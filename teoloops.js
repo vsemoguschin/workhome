c635f52926721d2e7314b7d01f623b18cf9f0e21
Alexey <sushak@mail.ru>
2021-10-06 17:22:37 +0300

    first commit

---
+++ teoloops.js
@@ -0,0 +1,19 @@
+let num = 50;
+// while (num < 55) {
+//   console.log('malo');
+//   num++;
+// }
+// console.log(num);
+
+////////
+
+do {
+  num++;
+  console.log("ещё");
+} while (num < 52);
+
+////////
+
+for (let i = 1; i < 8; i++) {
+  console.log(i);
+}
