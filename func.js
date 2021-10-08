3d2bdf6b212156e1bd5848191c1e09677a40623d
Alexey <sushak@mail.ru>
2021-10-08 18:50:59 +0300

    8.10

--- func.js
+++ func.js
@@ -3,8 +3,25 @@ let num = 222;
 console.log(num);
 function showMessage() {
   num = 20;
-//   console.log(num);
+  console.log(`внутри ${num}`);
 }
-console.log(num);
+console.log(`до вызова ${num}`);
 showMessage();
 // console.log(num);
+
+//////////
+
+declaration();
+function declaration() {
+  console.log("можно вызывать до объявления");
+}
+
+const expression = function () {
+  console.log("вызывается только после объявления");
+};
+expression();
+
+const arrowFunc = () => {
+  console.log('Стрелочная функция');
+};
+arrowFunc();
---
+++ metodsProp.js
@@ -0,0 +1,17 @@
+// const str = "SoMe tEXt";
+// const mass = [1, 2, 3];
+// console.log(`строка ${str}`);
+// console.log(`длина строки: ${str.length} символа`);
+// console.log(` длина массива: ${mass.length}`);
+// console.log(`в верхнем регистре: ${str.toUpperCase()}`);
+// console.log(`в нижнем регистре: ${str.toLowerCase()}`);
+// console.log(`индекс заданного символа ${str.indexOf("x")}`); //если -1 то нет такого символа
+// console.log(`вырезать заданный промежуток ${str.slice(5)}`); // отрицательные значения начинают отсчет с конца
+// console.log(`${str.substring()}`);
+// console.log(`${str.substr()}`);// первый аргумент - начало, второй - длинна
+
+const num = '12.58 dsadf';
+console.log(Math.round(num)) // округлить
+console.log(parseInt(num));
+console.log(parseFloat(num));
+
